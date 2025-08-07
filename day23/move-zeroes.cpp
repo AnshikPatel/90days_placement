b0fd0f492d208b2a321b18c5165149f1c35fdac8
Anshik <119244261+AnshikPatel@users.noreply.github.com>
2025-08-08 00:47:14 +0530

    Create move-zeroes.cpp

---
+++ day23/move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int count=0;
+        
+        for(int i=0;i<nums.size()-count;i++){
+            if(nums[i]==0){
+                nums.erase(nums.begin()+i);
+                i--;
+                nums.push_back(0);
+                count++;
+            }
+        }
+    }
+};
